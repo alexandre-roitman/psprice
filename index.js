4247bcbdcb5fd9b34e2760b2080a531237075b6c
Amit_Kaspi <akaspi@wix.com>
2016-09-07 18:56:48 +0300

    serve static html at /

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
+const staticPath = path.join(__dirname, '/public');
+const port = process.env.PORT || 8080;
+
 const search = require('./search');
 
+app.use(express.static(staticPath));
+
 app.get('/', (req, res) => {
+    res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+app.get('/search', (req, res) => {
     search(req.query.q)
         .then(result => {
             res.send(JSON.stringify(result));
         })
-    
 });
-app.listen();
+
+app.listen(port, () => {
+    console.log(`Server is listening at port ${port}`);
+});
